47de49d28a2d28d57bedf0c86104cea6a5e02b2d
Zeyad Osama Abd-El-Monem <zeyad.osama@hotmail.com>
2018-02-28 12:51:07 +0200

    Stack Optimized

--- stack.c
+++ stack.c
@@ -1,6 +1,6 @@
 /*
  *
- * C Program to Implement Constricted Stack Using Dynamic Memory Allocation
+ * C Program to Implement Constrict Stack Using Dynamic Memory Allocation
  *
  */
 
@@ -8,51 +8,56 @@
 #include <stdlib.h>
 
 #define TYPE int
-// typedef int TYPE;
 
 #define true 1
 #define false 0
 
-enum exceptionTags
-{
-	NO_EXCEPETIONS,
-	STACK_IS_EMPTY,
-	STACK_IS_FULL,
-	STACK_IS_DESTRUCTED
-};
+
+/* prototypes
+ */
+Stack * constructStack (int length);
+void destructStack (Stack*);
+int push (Stack* , TYPE value);
+TYPE pop (Stack*);
+int isFullStack (Stack*);
+int isEmptyStack (Stack*);
+void displayStack (Stack*);
+void searchStack (Stack* , TYPE target);
+void unloadStack (Stack*);
+void stackExeptionHandler (Stack*);
 
 
-/*
- * stack data type declaration
+/* defining enumeration for exceptions tags
+ */
+typedef enum 
+{ 
+	NO_EXCEPETIONS , 
+	STACK_IS_EMPTY , 
+	STACK_IS_FULL , 
+	STACK_IS_DESTRUCTED 
+} ExceptionTags;
+
+
+/* stack data-type declaration
  */
 typedef struct
 {
 	int top;
+	int length;
+
+	// pointer to array of a given data-type
+	// to be dynamically allocated by length through run-time
 	TYPE * items;
 
-	// holds exception -if there is- of the stack
+	// holds exception -if there- for a given stack
 	int exception;
-
-	int size;
-}Stack;
+} Stack;
 
 
-/*
- * prototypes
+
+/* expects one parameter: length of stack (array within the stack struct).
  */
-Stack * constructStack (int size);
-void destructStack (Stack * stack);
-int push (Stack * stack , TYPE value);
-TYPE pop (Stack * stack);
-int isFullStack (Stack * stack);
-int isEmptyStack (Stack * stack);
-void unloadStack (Stack * stack);
-void displayStack (Stack * stack);
-void searchStack (Stack * stack , TYPE target);
-void stackExeptionHandler (Stack * stack);
-
-
-Stack * constructStack (int size)
+Stack * constructStack (int length)
 {
 	// dynamically allocating memory for stack
 	Stack * stack = (Stack*) malloc(sizeof(Stack));
@@ -62,66 +67,92 @@ Stack * constructStack (int size)
         return NULL;
 
 	// dynamically allocating memory for items
-	stack->items = (TYPE*) malloc(size*sizeof(TYPE));
+	stack->items = (TYPE*) malloc(length*sizeof(TYPE));
 
 	// failure check
 	if(stack->items==NULL)
 	{
+		// freeing stack struct if array within the struct could not be allocated
 		free(stack);
         return NULL;
 	}
 
-	// initializing content
+	// initializing stack content
 	stack->top = 0;
-	stack->size = size;
+	stack->length = length;
 	stack->exception = STACK_IS_EMPTY;
 
 	return stack;
 }
 
 
-void destructStackStack (Stack * stack)
+/* expects one parameter: any given stack address.
+ */
+int destructStack (Stack * stack)
 {
 	stack->exception = STACK_IS_DESTRUCTED;
     free(stack->items);
     free(stack);
+
+	return STACK_IS_DESTRUCTED;
 }
 
 
+/* expects two parameter: any given stack address / to be inserted data.
+ * functionality: add data to top of stack.
+ */
 int push (Stack * stack , TYPE value)
 {
-	// check if already full
+	return isFullStack(stack) ? stack->exception = STACK_IS_FULL : stack->exception = NO_EXCEPETIONS
+
+	// check first if already full
 	if(isFullStack(stack))
         return stack->exception = STACK_IS_FULL;
 
 	// increment top then push
 	stack->items[stack->top++]=value;
 
+	// no thrown exceptions
 	return stack->exception = NO_EXCEPETIONS;
 }
 
 
+/* expects one parameter: any given stack address.
+ * functionality: remove data from top of stack.
+ * could be assigned to variables.
+ */
 TYPE pop (Stack * stack)
 {
-	// check if already empty
+	return isEmptyStack(stack) ? stack->exception = STACK_IS_EMPTY : stack->items[--stack->top];
+
+	// check first if already empty
 	if(isEmptyStack(stack))
         return stack->exception = STACK_IS_EMPTY;
+
+    // no thrown exceptions
+    stack->exception = NO_EXCEPETIONS;
 
 	// decrement top then pop
 	return stack->items[--stack->top];
 }
 
 
-void unloadStack (Stack * stack)
+/* expects one parameter: any given stack address.
+ * functionality: remove all content of a given stack
+ */
+int unloadStack (Stack * stack)
 {
 	while (!isEmptyStack(stack))
 		pop(stack);
+
+	// throw exception to assure no popping
+	return stack->exception = STACK_IS_EMPTY;
 }
 
 
 void displayStack (Stack * stack)
 {
-	Stack * cop = constructStack(stack->size);
+	Stack * cop = constructStack(stack->length);
 
 	printf("[ ");
 
@@ -140,15 +171,19 @@ void displayStack (Stack * stack)
 }
 
 
+/* boolean check
+ */
 int isFullStack (Stack * stack)
 {
-	return (stack->top < stack->size) ? false : true;
+	return (stack->top < stack->length) ? false : true;
 }
 
 
+/* boolean check
+ */
 int isEmptyStack (Stack * stack)
 {
-	return (stack->top==0) ? true : false;
+	return (stack->top == 0) ? true : false;
 }
 
 
@@ -161,20 +196,21 @@ void searchStack (Stack * stack , TYPE target)
 	{
 		for (j=0; j<i ; j++)
 		{
-
+			// TO DO CODE HERE
 		}
 	}
 }
 */
 
 
-/*
- * handles exceptions that may occur in a given stack
+/* expects one parameter: any given stack address.
+ * functionality: handles exceptions that may occur in a given stack
  */
 void stackExeptionHandler (Stack * stack)
 {
 	switch(stack->exception)
 	{
+		// no thrown exceptions
 		case NO_EXCEPETIONS:
 			break;
 
